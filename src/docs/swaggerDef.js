183aceb3b264a87c58ac212dcc6896f22ef0d1aa
PraveenKumar <praveen.senthi03@gmail.com>
2022-08-09 09:46:54 +0530

    swagger updates

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -2,18 +2,21 @@
    docs Name : swaggerDef
 */
 
-const { version } = require('../../package.json');
+const { version } = require("../../package.json");
 
 const swaggerDef = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'Paradigm',
-    description: '07/05/2021',
+    title: "Movies-fortunesoft",
+    description: "08/08/2022",
     version,
   },
   servers: [
     {
-      url: 'http://localhost:3000/v1', // change url based on (local/production)
+      url: "http://localhost:3000/v1", // change url based on (local/production)
+    },
+    {
+      url: "https://movies-list-fortunesoft.herokuapp.com/v1",
     },
   ],
 };
